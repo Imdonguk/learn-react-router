65c8c927e99667362f67bee3bb47418acc9b8f7e
Imdonguk <com8157@naver.com>
2019-03-26 16:53:43 +0900

    Add : URL의 query를 해석

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import queryString from 'query-string';
 
-const About = ({ match }) => {
+const About = ({ location, match }) => {
+    const query = queryString.parse(location.search);
+    const detail = query.detail;
+
     return (
         <div>
             <h2>About {match.params.name}</h2>
+            {detail && '쿼리 detail이 true일 때만 렌더링'}
         </div>
     );
 };
