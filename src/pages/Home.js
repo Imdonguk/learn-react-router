74a9756514f4e0edcd7e2750849b757a1a72df3e
Imdonguk <com8157@naver.com>
2019-03-28 02:45:45 +0900

    Add : history의 push메서드를 사용해서 라우트 이동

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Home = () => {
+const Home = ({ history }) => {
     return (
         <div>
             <h2>HOME</h2>
+            <button onClick={() => { history.push('/about') }}>클릭하면 About으로 이동</button><br />
+            <button onClick={() => { history.push('/posts') }}>클릭하면 포스트로 이동</button><br />
+            <button onClick={() => { history.push('/me') }}>클릭하면 마이페이지로 이동</button><br />
+            <button onClick={() => { history.push('/login') }}>클릭하면 로그인페이지 이동</button><br />
         </div>
     );
 };
